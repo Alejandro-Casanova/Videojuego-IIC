13f82097651e01444eff84550e591da21d69a4e1
Diego-PazM <DiegoMP527@gmail.com>
2021-05-15 15:28:43 +0100

    Cambios Menores

--- src/Interaccion.h
+++ src/Interaccion.h
@@ -4,6 +4,8 @@ class Personaje;
 class Caja;
 class Pared;
 class Obstaculo;
+class Proyectil;
+class Enemigo;
 
 class Interaccion
 {
@@ -11,5 +13,8 @@ public:
 	static void rebote(Personaje& p, Caja c);
 	static bool rebote(Personaje& e, Pared p);
 	static void rebote(Personaje& p, Obstaculo o);
+	static void impacto(Proyectil& p, Obstaculo);
+	static void impacto(Proyectil& p, Enemigo);
+	static void impacto(Proyectil& p, Pared);
 };
 
